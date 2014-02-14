2dfdbcd8216160bfc2cdd816be6f0f39613f8aca
Abhishek Parihar <abhishek@weboniselab.com>
2014-02-14 11:50:08 +0530

    changed code to make project as library and can be used with android projects

--- src/weboapps/crashsense/MyApplication.java
+++ src/weboapps/crashsense/MyApplication.java
@@ -34,10 +34,10 @@ import android.util.Log;
 @SuppressLint("SimpleDateFormat")
 public class MyApplication extends Application {
 
-	private final String TAG =getClass().getName();
-	private UncaughtExceptionHandler defaultUEH;
-	private String strFilename;
-	
+	protected final String TAG =getClass().getName();
+	protected UncaughtExceptionHandler defaultUEH;
+	protected String strFilename;
+
 	/**
 	 * Constructor to initialize the base UncaughtExceptionHandler object.
 	 */
@@ -45,20 +45,20 @@ public class MyApplication extends Application {
 		defaultUEH = Thread.getDefaultUncaughtExceptionHandler(); 
 		Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
 	} 
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 	}
 
-	private Thread.UncaughtExceptionHandler uncaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
+	protected Thread.UncaughtExceptionHandler uncaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
 
 		@Override
 		public void uncaughtException(Thread thread, Throwable ex) {
 			StringWriter sw = new StringWriter(); 
 			PrintWriter pw = new PrintWriter(sw); 
 			ex.printStackTrace(pw);
-			
+
 			/*Store information about the device and the crash log in an array of string .*/
 			String[] params = new String[7];
 			params[0] = Constants.OS;
@@ -67,7 +67,7 @@ public class MyApplication extends Application {
 			params[3] = ex.getClass().toString();  
 			params[4] = ex.getMessage();
 			params[5] = sw.toString(); 
-			params[6] = Constants.API_KEY;
+			params[6] = getKey();
 
 			/*	Check if the network is available,
 			 *  if the network is available then upload the file on the server,
@@ -87,7 +87,7 @@ public class MyApplication extends Application {
 	/**
 	 * @return string that has the detail of the device type.
 	 */
-	private String getDeviceType() { 
+	protected String getDeviceType() { 
 		StringBuilder mStringBuilder= new StringBuilder();
 		mStringBuilder.append(android.os.Build.MANUFACTURER+" "+android.os.Build.MODEL);
 		return mStringBuilder.toString();
@@ -114,13 +114,13 @@ public class MyApplication extends Application {
 			Log.i(TAG,"File operation: Crash logged in the file status - Incomplete."); 
 		} 
 	}
-	
+
 	/**
 	 * @return string which further is used as a file name.
 	 * 	This function gets the local machine day, date and time and combining all those
 	 * 	creates a file name. 
 	 */
-	private String getFileName() {
+	protected String getFileName() {
 		SimpleDateFormat sdf= new SimpleDateFormat("EEEdMMMyyyy-HH:mm:ss");  
 		Date date = new Date();
 		String strFilename= sdf.format(date); 
@@ -128,14 +128,14 @@ public class MyApplication extends Application {
 		Log.i(TAG,"File Name: "+strFilename);
 		return strFilename;
 	}
-	
+
 	/**
 	 * This function first reads a file named "filename.txt" where a list of filenames 
 	 * 	are stored. This function reads the top name from the list and get that file 
 	 * 	content. Further this function parsed the string content received and stores 
 	 * 	it in an array which is passed to the web service for uploading.
 	 */
-	private void uploadCrashLogFromFile() { 
+	protected void uploadCrashLogFromFile() { 
 		try {
 			FileInputStream inputStream = openFileInput("filename.txt");
 			if(inputStream!=null) {
@@ -159,12 +159,12 @@ public class MyApplication extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * @param filename name of the file whose content you want to read.
 	 * @return string which has the content of the file.
 	 */
-	private String getFileContent(String filename) {
+	protected String getFileContent(String filename) {
 		String ret = "";
 		try {
 			InputStream inputStream = openFileInput(filename);
@@ -186,18 +186,18 @@ public class MyApplication extends Application {
 		}
 		return ret;
 	}
-	
+
 	/**
 	 * @param strFileContent string that you have to parse.
 	 * @return array of string 
 	 * 	This function returns an array of string parsed from a single string. 
 	 */
-	private String[] getStringArrayFromFileContent(String strFileContent) {
+	protected String[] getStringArrayFromFileContent(String strFileContent) {
 		String [] params= new String[7];
 		params = strFileContent.split("---");
 		return params;
 	}
-	
+
 	/**
 	 * This function is called when the web service task is successful and we get
 	 * 	correct response. After the response is received it checks if the uploaded 
@@ -212,12 +212,12 @@ public class MyApplication extends Application {
 		}
 		uploadCrashLogFromFile();
 	}
-	
+
 	/**
 	 * 	This function deletes the file from the memory and also the file entry from 
 	 * 	the "filename.txt" file.
 	 */
-	private void clearFileRecord() {
+	protected void clearFileRecord() {
 		Log.v(TAG,"File operation: Cleaning file record. File name is : "+strFilename);  
 		File file = new File(Constants.FILE_URL+strFilename);
 		boolean deleted = file.delete();
@@ -228,7 +228,7 @@ public class MyApplication extends Application {
 			Log.i(TAG,"File operation: File was not deleted successfully"); 
 		}
 	}
-	
+
 	/**
 	 * @param file is the filename which you have to modify
 	 * @param lineToRemove is the content you want to delete.
@@ -236,35 +236,39 @@ public class MyApplication extends Application {
 	 */
 	public void deleteFileEntryFromFilename(String file, String lineToRemove) {
 		try {
-		  File inFile = new File(file);
-		  if (!inFile.isFile()) {
-		    Log.i(TAG,"File operation: there is no such file as : "+file);
-		    return;
-		  }
-		  File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
-		  BufferedReader br = new BufferedReader(new FileReader(file));
-		  PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
-		  String line = null;
-		  while ((line = br.readLine()) != null) {
-		    if (!line.trim().equals(lineToRemove)) {
-		      pw.println(line);
-		      pw.flush();
-		    }
-		  }
-		  pw.close();
-		  br.close();
-		  if (!inFile.delete()) {
-			Log.i(TAG,"File operation: could not delete the file");
-		    return;
-		  }
-		  if (!tempFile.renameTo(inFile))
-			Log.i(TAG,"File operation: could not rename the file");
+			File inFile = new File(file);
+			if (!inFile.isFile()) {
+				Log.i(TAG,"File operation: there is no such file as : "+file);
+				return;
+			}
+			File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				if (!line.trim().equals(lineToRemove)) {
+					pw.println(line);
+					pw.flush();
+				}
+			}
+			pw.close();
+			br.close();
+			if (!inFile.delete()) {
+				Log.i(TAG,"File operation: could not delete the file");
+				return;
+			}
+			if (!tempFile.renameTo(inFile))
+				Log.i(TAG,"File operation: could not rename the file");
 		}
 		catch (FileNotFoundException ex) {
-		  ex.printStackTrace();
+			ex.printStackTrace();
 		}
 		catch (IOException ex) {
-		  ex.printStackTrace();
+			ex.printStackTrace();
 		}
+	}
+
+	protected String getKey(){
+		return "";
 	}
 }
