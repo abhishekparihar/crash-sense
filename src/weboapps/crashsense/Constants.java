1cff622384ac301bbb5421da1ecae0b6270511e7
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2014-03-11 19:55:55 +0530

    removed unused files

--- src/weboapps/crashsense/Constants.java
+++ src/weboapps/crashsense/Constants.java
@@ -6,7 +6,8 @@ package weboapps.crashsense;
  */
 public class Constants {
 	public static final String OS = "Android";
-	public static final String BASE_URL = "http://stage106.weboapps.com";
+//	public static final String BASE_URL = "http://stage106.weboapps.com";
+	public static final String BASE_URL = "http://beta.erreport.io";
 	public static final String API_KEY = "2f418511c853c7c0b1d168d94f97ab2c";
 	public static final String FILE_URL = "/data/data/weboapps.crashsense/files/";
 	public static final String OS_VERSION = android.os.Build.VERSION.RELEASE;
